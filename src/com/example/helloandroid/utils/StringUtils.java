030387260c39843bb6f1c26ec1b2297f43ec8c89
careyrp <careyr@computer.org>
2012-02-25 12:03:56 -0800

    fix warning

--- src/com/example/helloandroid/utils/StringUtils.java
+++ src/com/example/helloandroid/utils/StringUtils.java
@@ -26,7 +26,7 @@ public class StringUtils {
 	
 	public static String join(Collection<?> s, String delimiter) {
 	     StringBuilder builder = new StringBuilder();
-	     Iterator iter = s.iterator();
+	     Iterator<?> iter = s.iterator();
 	     while (iter.hasNext()) {
 	         builder.append(iter.next());
 	         if (!iter.hasNext()) {
