8dc5a1572c283ba0e02239bbaf89033562b6a551
careyrp <careyr@computer.org>
2012-02-25 12:01:39 -0800

    cleanup imports

--- src/com/example/helloandroid/fragments/RouteListFragment.java
+++ src/com/example/helloandroid/fragments/RouteListFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.ContentUris;
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ListView;
