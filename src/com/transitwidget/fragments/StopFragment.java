797c5bfc360097f824ec18b397865c91804c2613
John Roepke <john@justjohn.us>
2012-04-22 21:33:09 -0400

    Add upcoming predictions to stop fragment.

--- src/com/transitwidget/fragments/StopFragment.java
+++ src/com/transitwidget/fragments/StopFragment.java
@@ -11,26 +11,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import com.transitwidget.R;
 import com.transitwidget.api.NextBusAPI;
 import com.transitwidget.feed.model.BusPrediction;
 import com.transitwidget.feed.model.Stop;
-import com.transitwidget.prefs.NextBusObserverConfig;
 import com.transitwidget.utils.TimeUtils;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class StopFragment extends Fragment {
 	public static final String ARG_STOP_TAG = "stop";
 	public static final String ARG_ROUTE_TAG = "route";
 	public static final String ARG_DIRECTION_TAG = "direction";
 	public static final String ARG_AGENCY_TAG = "agency";
+    
+    public final static String ITEM_TITLE = "title";  
+    public final static String ITEM_CAPTION = "caption";
 	
 	private TextView mStopLabel;
     private TextView mNextTime;
     private TextView mAbsoluteTime;
+    private ListView mMorePredictions;
     
     private Stop mStop;
     private String mDirection;
@@ -54,6 +60,7 @@ public class StopFragment extends Fragment {
 		mStopLabel   = (TextView) view.findViewById(R.id.stop);
         mNextTime    = (TextView) view.findViewById(R.id.next_time);
         mAbsoluteTime = (TextView) view.findViewById(R.id.absolute_time);
+        mMorePredictions = (ListView) view.findViewById(R.id.more_predictions);
 	}
 	
 	@Override
@@ -88,24 +95,41 @@ public class StopFragment extends Fragment {
                 
             } else {
                 BusPrediction nextPrediction = predictions.get(0);
-                
-                BusPrediction secondPrediction = new BusPrediction();
-                if (predictions.size() > 1) {
-                    secondPrediction = predictions.get(1);
-                }
+                predictions.remove(0);
                 
                 String nextTime = TimeUtils.formatTimeOfNextBus(nextPrediction.getEpochTime());
                 String absoluteTime = TimeUtils.formatAbsoluteTimeOfNextBus(nextPrediction.getEpochTime());
 
                 mNextTime.setText(nextTime);
                 mAbsoluteTime.setText(absoluteTime);
+                
+                List<Map<String,?>> predictionsList = new LinkedList<Map<String,?>>();  
+                    
+                for (BusPrediction prediction : predictions) {
+                    long predictionTime = prediction.getEpochTime();
+                    
+                    String timeUntil = TimeUtils.formatTimeOfNextBus(predictionTime);
+                    String timeAt = TimeUtils.formatAbsoluteTimeOfNextBus(predictionTime);
+                    
+                    Map<String,String> item = new HashMap<String, String>();
+                    item.put(ITEM_TITLE, timeUntil);
+                    item.put(ITEM_CAPTION, timeAt);
+                    
+                    predictionsList.add(item);
+                }
+
+                String[] mapping = { ITEM_TITLE, ITEM_CAPTION };
+                int[] fields = { R.id.list_title, R.id.list_caption };
+                
+                if (getActivity() != null) {
+                    mMorePredictions.setAdapter(new SimpleAdapter(getActivity(), predictionsList, R.layout.list_complex, mapping, fields));
+                }
             }
         }
     }
     
     class UpdateRunnable implements Runnable {
         public void run() {
-            
             new UpdateTask().execute("");
             
             // mHandler.postDelayed(new UpdateRunnable(), 30000);
